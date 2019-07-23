db9f0cc77bddebfdd7cb9d2c71c2e0fe01cd9556
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-07-23 15:18:37 -0300

    Usando o Migrations, para gerar o banco de dados

---
+++ src/DevIO.UI.Site/Migrations/20190723180533_Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevIO.UI.Site.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "Alunos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "Alunos");
+        }
+    }
+}
--- src/DevIO.UI.Site/Models/Aluno.cs
+++ src/DevIO.UI.Site/Models/Aluno.cs
@@ -15,5 +15,7 @@ namespace DevIO.UI.Site.Models
         public string Nome { get; set; }
         public string Email { get; set; }
         public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+
     }
 }
