7f0cb519f98962645133ce5032486bf9e4d859ec
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-07-23 13:53:16 -0300

    Conectando com o banco ORACLE.

---
+++ src/DevIO.UI.Site/Data/MyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.UI.Site.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
--- src/DevIO.UI.Site/Startup.cs
+++ src/DevIO.UI.Site/Startup.cs
@@ -7,18 +7,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DevIO.UI.Site
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddTransient<IPedidoRepository, PedidoRepository>();
+            services.AddDbContext<MyDBContext>(options => options.UseOracle(Configuration.GetConnectionString("MyDbContext")));
+
 
         }
 
